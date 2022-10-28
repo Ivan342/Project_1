1b6197897b76417a1f017fca2d4e00cb0b634542
Ivan_1 <mail@mai.com>
2022-10-28 12:08:23 +0300

    Смайлик

--- 1.draw.py
+++ 1.draw.py
@@ -6,14 +6,34 @@ pygame.init()
 FPS = 30
 screen = pygame.display.set_mode((400, 400))
 
-rect(screen, (255, 0, 255), (100, 100, 200, 200))
-rect(screen, (0, 0, 255), (100, 100, 200, 200), 5)
-polygon(screen, (255, 255, 0), [(100,100), (200,50),
-                               (300,100), (100,100)])
-polygon(screen, (0, 0, 255), [(100,100), (200,50),
-                               (300,100), (100,100)], 5)
-circle(screen, (0, 255, 0), (200, 175), 50)
-circle(screen, (255, 255, 255), (200, 175), 50, 5)
+BLACK = (0, 0, 0)
+GRAY = (100, 100, 100)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+CYAN = (0, 255, 255)
+MAGENTA = (255, 0, 255)
+
+background = GRAY
+screen.fill(background)
+
+pygame.draw.ellipse(screen, YELLOW, (50, 50, 300, 300))
+pygame.draw.ellipse(screen, BLACK, (50, 50, 300, 300), 3)
+pygame.draw.ellipse(screen, RED, (100, 145, 55, 55))
+pygame.draw.ellipse(screen, BLACK, (100, 145, 55, 55), 2)
+pygame.draw.ellipse(screen, RED, (240, 155, 50, 50))
+pygame.draw.ellipse(screen, BLACK, (240, 155, 50, 50), 2)
+
+pygame.draw.ellipse(screen, BLACK, (100+55/2-30/2, 145+55/2-30/2, 30, 30))
+pygame.draw.ellipse(screen, BLACK, (240+50/2-25/2, 155+50/2-25/2, 25, 25))
+
+pygame.draw.rect(screen, BLACK, (120, 270, 160, 30))
+
+line(screen, BLACK, (60, 60), (160, 150), 30)
+line(screen, BLACK, (360, 60), (240, 150), 30)
+
 
 pygame.display.update()
 clock = pygame.time.Clock()
