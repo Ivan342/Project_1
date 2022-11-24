ca898b77fb334d820b2b7a5f317a3cf4c6cd3c26
Ivan_1 <mail@mai.com>
2022-11-24 23:29:35 +0300

    Движущиеся цели в пушке

--- Lab_2/Lab_2_Canon.py
+++ Lab_2/Lab_2_Canon.py
@@ -22,7 +22,7 @@ HEIGHT = 600
 
 
 def draw_rectangle(x, y, width, height, color, rotation=0):
-    """Draw a rectangle, centered at x, y.
+    """Draw a rotated rectangle, centered at x, y.
 
     Arguments:
       x (int/float):
@@ -35,6 +35,9 @@ def draw_rectangle(x, y, width, height, color, rotation=0):
         The height of the rectangle.
       color (str):
         Name of the fill color, in HTML format.
+      rotation (int/float):
+        Rotation angle of the rectangle
+
     """
     points = []
 
@@ -88,11 +91,11 @@ class Ball:
         self.x += self.vx
         self.y -= self.vy
 
-        self.vy -= 1
+        self.vy -= 1.6
 
         if self.y > HEIGHT - 50:
             self.vy = -0.5 * self.vy
-            if abs(self.vy) > 2:
+            if abs(self.vy) > 2.7:
                 self.y -= self.vy
                 self.vx = 0.8 * self.vx
             else:
@@ -103,6 +106,7 @@ class Ball:
             self.x += self.vx
 
     def draw(self):
+        """Отрисовка объекта"""
         pygame.draw.circle(
             self.screen,
             self.color,
@@ -126,6 +130,7 @@ class Ball:
 
 class Gun:
     def __init__(self, screen):
+        """Конструктор класса Gun"""
         self.screen = screen
         self.f2_power = 10
         self.f2_on = 0
@@ -134,6 +139,7 @@ class Gun:
         self.length = 40
 
     def fire2_start(self, event):
+        """Начинает накопление заряда выстрела"""
         self.f2_on = 1
 
     def fire2_end(self, event):
@@ -166,12 +172,14 @@ class Gun:
             self.color = GREY
 
     def draw(self):
+        """Отрисовка объекта"""
         draw_rectangle(40 + self.length / 2 * cos(self.an),
                        450 + self.length / 2 * sin(self.an),
                        self.length, 15, self.color,
                        -(self.an / pi) * 180)
 
     def power_up(self):
+        """Накопление заряда выстрела (при задержке кнопки мыши)"""
         if self.f2_on:
             if self.f2_power < 50:
                 self.f2_power += 1
@@ -186,21 +194,24 @@ class Gun:
 
 
 class Target:
-    points = 0
+    """Класс для мишени (цели)"""
+    points = 1
     live = 1
 
     def __init__(self):
         """ Инициализация новой цели. """
-        x = self.x = randint(600, 780)
+        x = self.x = randint(450, 700)
         y = self.y = randint(300, 550)
-        r = self.r = randint(10, 40)
+        r = self.r = randint(20, 40)
+        vx = self.vx = 0
+        vy = self.vy = randint(-10, 10)
+        freq = self.freq = randint(1000, 2000) / 1000
+        amp = self.amp = randint(8, 20)
+        time = self.time = 0
         color = self.color = RED
 
-    def hit(self, points=1):
-        """Попадание шарика в цель."""
-        self.points += points
-
     def draw(self):
+        """Отрисовка цели"""
         pygame.draw.circle(
             screen,
             self.color,
@@ -208,25 +219,43 @@ class Target:
             self.r
         )
 
+    def move(self):
+        """Переместить цель по прошествии единицы времени.
+
+        Метод описывает перемещение цели за один кадр перерисовки. То есть, обновляет значения
+        self.x и self.y с учетом скоростей self.vx и self.vy (размер окна 800х600).
+        """
+        self.y += self.vy
+        self.x += self.amp * sin(self.freq * self.time / 10)
+        self.time += 3
+
+        if self.y > HEIGHT - 50:
+            self.vy = -self.vy
+        if self.y < 50:
+            self.vy = -self.vy
+
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 bullet = 0
 balls = []
+targets = [Target(), Target()]
+global score
+score = 0
 
 clock = pygame.time.Clock()
 gun = Gun(screen)
-target = Target()
+
 finished = False
 
 while not finished:
     screen.fill(WHITE)
     gun.draw()
-    target.draw()
+    for t in targets:
+        t.draw()
     for b in balls:
         b.draw()
         b.live -= 1
-        # print(b.live)
     pygame.display.update()
 
     clock.tick(FPS)
@@ -239,21 +268,27 @@ while not finished:
             gun.fire2_end(event)
         elif event.type == pygame.MOUSEMOTION:
             gun.targetting(event)
-
+    for t in targets:
+        t.move()
     for b in balls:
         b.move()
-        if b.hittest(target) and target.live:
-            print("Hit")
-            target.live = 0
-            target.hit()
-            # target.new_target()
-            del target
-            balls.remove(b)
-            target = Target()
+        for t in targets:
+            if b.hittest(t) and t.live:
+                score += 1
+                print("Hit! Your score is now ", score)
+                t.live = 0
+                # target.new_target()
+                targets.remove(t)
+                balls.remove(b)
+                targets.append(Target())
         if b.live < 0:
             balls.remove(b)
             # print("del")
 
     gun.power_up()
+
+    if score == 5:
+        print("You won!")
+        finished = True
 
 pygame.quit()
